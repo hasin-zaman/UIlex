04e1b545efea53317f9be0e7b26c74903e33415d
Hasin <121337662+hasin-zaman@users.noreply.github.com>
2024-11-09 14:14:37 -0800

    fixes: Header

--- src/custom-components/Header.jsx
+++ src/custom-components/Header.jsx
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <header className="absolute top-0 left-0 w-full h-12 flex justify-between items-center pr-4 z-10">
       <SidebarTrigger />
-      <div className="md:h-12 w-full h-8">
+      <div className="lg:h-12 sm:h-10 w-full h-8">
         <TextHoverEffect text="By The People. For The People." />
       </div>
       <StarButton />
